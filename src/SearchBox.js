28baa0d56a8964ee2acb53eb43292ed7a3fd63a0
Rodrigo Santos <rr.santos1232@gmail.com>
2020-03-03 18:58:17 +0000

    Base Files

---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const SearchBox = () => {
+    return (
+        <Fragment>
+            <input type='search' placeholder='Search a Robot...' />
+        </Fragment>
+    );
+}
+
+export default SearchBox;
